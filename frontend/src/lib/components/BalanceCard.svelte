7c63b3c15b53a3136d0e957794706c997f407857
Johann Homonnai <j.homonnai@icloud.com>
2025-04-05 12:38:10 -0300

    ref(BalanceCard): add accountName prop, use stat from daisyui

--- frontend/src/lib/components/BalanceCard.svelte
+++ frontend/src/lib/components/BalanceCard.svelte
@@ -1,19 +1,21 @@
 <script lang="ts">
 	import { formatCurrency } from "$lib";
 
-	type Props = { balance: number };
+	type Props = { balance: number; accountName: string };
 
-	let { balance }: Props = $props();
+	let { balance, accountName = "Checking" }: Props = $props();
 
-	const balanceColor = balance >= 0 ? "text-success" : "text-error";
+	let balanceColor: "text-success" | "text-warning" | "text-error" = $state("text-warning");
+
+	if (balance > 0) {
+		balanceColor = "text-success";
+	} else if (balance < 0) {
+		balanceColor = "text-error";
+	}
 </script>
 
-<div class="card bg-base-100 mb-6 shadow-xl">
-	<div class="card-body">
-		<div class="flex items-center justify-between">
-			<h3 class="card-title">Account Balance</h3>
-			<div class="badge badge-primary">Checking</div>
-		</div>
-		<p class="font-mono text-3xl font-bold {balanceColor}">{formatCurrency(balance)}</p>
-	</div>
+<div class="stat">
+	<div class="stat-title">Account Balance</div>
+	<p class="stat-value {balanceColor}">{formatCurrency(balance)}</p>
+	<div class="stat-desc">{accountName}</div>
 </div>
